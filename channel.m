4ff39c5383ec47d86af7a619c56258cfda201f3e
andylithia <thelithcore@gmail.com>
2022-04-09 11:14:56 -0400

    Channel Finished

--- channel.m
+++ channel.m
@@ -1,5 +1,6 @@
 function sigOut = channel(sig, setup)
     % SETTINGS:
+    fs       = setup.fs;
     regSNR   = setup.regSNR;    % Requested Regulated SNR (compared to the RX signal), log Scale
     fadeType = setup.fadeType;  % Currently Supported:
                                 %   'rayleigh':      Rayleigh Fade
@@ -7,22 +8,25 @@ function sigOut = channel(sig, setup)
                                 %   'flat':          Uniform Attenuation
     rayleighVelocity = setup.rayleighVelocity;
     flatAttenuation  = setup.flatAttenuation;       % given in db20, should be a positive number
-   
+    multiPathSetup   = setup.multiPathSetup;        % Multi-Path Kernel
     % Apply Multi-Path (done using convolve)
-    % TBD
-    
+    %% sig = conv(sig,kernel);
+    for i = 1:length(multiPathSetup(:,1))
+        amp = multiPathSetup(i,1);
+        pos = multiPathSetup(i,2)*fs;
+        sig = sig + amp.*[zeros(1,pos),sig(1:length(sig)-pos)];
+    end
+
     % Apply Fading
     switch fadeType 
         case 'rayleigh'
             [h, rayleighTc, rayleighTs] = Rayleigh(length(sig(:)), rayleighVelocity);
-            rayleighMaxpercoh = floor(Tc/Ts);
             sig = sig.*h;
         case 'rayleigh_pdp'
             [h, rayleighTc, rayleighTs, rayleighLc] = RayleighPDP(length(sig(:)), rayleighVelocity); 
-            rayleighMaxpercoh = floor(Tc/Ts);
             sig = sig.*h;
         case 'flat'
-            sig = sig*10*(-flatAttenuation/20);
+            sig = sig*10^(-flatAttenuation/20);
         otherwise
             warning('No fade type specified, no fading applied');
     end
