862df08b8a690ba0130b04a28b4d63636d555952
jhz701 <84003266+jhz701@users.noreply.github.com>
2022-04-12 00:35:16 -0400

    Path loss model

---
+++ FSPL.m
@@ -0,0 +1,27 @@
+% Free space path loss model
+% Asigout/Asigin = (c/2r)*(1/w)
+% fs is sampling frequency, fmin and fmax are the minimum and maximum
+% frequecny that the algorithm is applied to. 
+function sig_out = FSPL(sig_in,r,fs)
+    c = 3e8;
+    coef = c/(2*r);
+    len = length(sig_in);
+    time = (1/fs)*(len-1); % signal duration
+    RBW = 1/time; % frequency resolution
+    spectrum = fft(coef*sig_in);
+    if(rem(len,2)) % if the lengthe of the sequence is odd 
+        for i = 2:1:(len-1)/2
+            w = 2*pi*(i-1)*RBW;
+            spectrum(i) = (1/w)*specturm(i);
+            spectrum(len-i+2) = (1/w)*spectrum(len-i+2);
+        end
+    else % the length of the sequence is even
+        for i = 2:1:len/2
+            w = 2*pi*(i-1)*RBW;
+            spectrum(i) = (1/w)*spectrum(i);
+            spectrum(len-i+2) = (1/w)*spectrum(len-i+2);
+        end
+        spectrum(len/2+1) = (1/(2*pi*(len/2)*RBW))*spectrum(len/2+1);
+    end
+    sig_out = ifft(spectrum);
+end
---
+++ FSPL_test.m
@@ -0,0 +1,73 @@
+%% symbol gen
+n = 10; %10th order derivative
+fs = 100e9; %sampling frequency
+fc = 5e9; % center frequency
+frame = 10e-9;% 10ns frame
+an = 1e-114;% scaling factor
+frame_num = 1;% 100 frames of data
+RBW = 1e-6/(frame*frame_num); %resolution bw in MHz
+pulse_duration = 1.5e-9;
+pulse = [];
+random_data = [];
+tguard = 3.5e-9;
+tstep = 100e-12;
+for i = 1:frame_num
+    data = randi(32)-1;
+    data_bits = de2bi(data,5,'left-msb');
+    pulse = [pulse DMPPM_symbol_gen(data_bits,tguard,tstep,frame,n,fs,fc,pulse_duration,an)];
+    random_data = [random_data data];
+end
+time = 0:1/fs:frame_num*frame-1/fs;
+%% original pulse plot
+figure
+plot(time,pulse);
+title('Gaussian Pulse');
+xlabel('Time(s)');
+ylabel('Amplitude');
+%% pulse after path loss
+r = 1; %distance is 1m
+pulse_fspl = FSPL(pulse,r,fs);
+figure;
+plot(time,pulse_fspl);
+title('Gaussian Pulse after FSPL')
+xlabel('Time(s)');
+ylabel('Amplitude');
+%% PSD plot
+figure
+L=length(pulse);
+X = fftshift(fft(pulse));
+Pxx=X.*conj(X)/(L*L); %computing power with proper scaling
+Pxx = Pxx/50; % power is defined with a 50 ohm load
+f = fs*(-L/2:1:L/2-1)/L; %Frequency Vector
+f = f(L/2+1:end);
+Pxx = 2*Pxx(L/2+1:end); % positive freqency
+PSD1 = Pxx*1e6*1e3/(1/(frame*frame_num)); % PSD has the unit of dBm/MHz
+s = stem(f*1e-9,10*log10(PSD1));
+s.BaseValue = -400;
+title('PSD of the Original Pulse');
+xlabel(['Frequency (GHz), RBW=',num2str(RBW),'MHz'])
+ylabel('Magnitude dBm/MHz');
+xlim([0 10.6]);
+ylim([-300 -30]);
+%% FSPL PSD plot
+figure
+L=length(pulse_fspl);
+X = fftshift(fft(pulse_fspl));
+Pxx=X.*conj(X)/(L*L); %computing power with proper scaling
+Pxx = Pxx/50; % power is defined with a 50 ohm load
+f = fs*(-L/2:1:L/2-1)/L; %Frequency Vector
+f = f(L/2+1:end);
+Pxx = 2*Pxx(L/2+1:end); % positive freqency
+PSD2 = Pxx*1e6*1e3/(1/(frame*frame_num)); % PSD has the unit of dBm/MHz
+s = stem(f*1e-9,10*log10(PSD2));
+s.BaseValue = -400;
+title('PSD of FSPL pulse');
+xlabel(['Frequency (GHz), RBW=',num2str(RBW),'MHz'])
+ylabel('Magnitude dBm/MHz');
+xlim([0 10.6]);
+ylim([-300 -30]);
+%% Sanity check
+semilogx(f*1e-9,10*log10(PSD1./PSD2));
+title('Path Loss');
+xlabel(['Frequency (GHz), RBW=',num2str(RBW),'MHz'])
+ylabel('Path Loss dBm/MHz');
